a2fa62eae281f87f90a96b87acb8268d4bd79a16
arora <aroramadhav65@gmail.com>
2022-01-22 12:44:24 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:vatika/screens/Login/LoginScreen.dart';
 import 'package:vatika/screens/Theme/provider/theme_provider.dart';
-import 'screens/Splash/Splash_Screen.dart';
+import 'package:vatika/styles/app_color.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,28 +20,28 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
-  // Widget build(BuildContext context) {
-  //   return MaterialApp(
-  //     debugShowCheckedModeBanner: false,
-  //     title: 'Vatika',
-  //     theme: ThemeData(
-  //       primarySwatch: Colors.blue,
-  //     ),
-  //     home: SplashScreen(),
-  //   );
-  // }
   Widget build(BuildContext context) => ChangeNotifierProvider(
         create: (context) => ThemeProvider(),
         builder: (context, _) {
           final themeProvider = Provider.of<ThemeProvider>(context);
-
           return MaterialApp(
             title: 'Vatika',
             debugShowCheckedModeBanner: false,
             themeMode: themeProvider.themeMode,
             theme: MyThemes.lightTheme,
             darkTheme: MyThemes.darkTheme,
-            home: SplashScreen(),
+            home: AnimatedSplashScreen(
+              splash: Image.asset(
+                '../assets/images/Vatika.gif',
+              ),
+              animationDuration: const Duration(milliseconds: 1000),
+              nextScreen: const LoginScreen(),
+              splashTransition: SplashTransition.sizeTransition,
+              backgroundColor: AppColor.primary1,
+              duration: 3000,
+              splashIconSize: 400,
+              centered: true,
+            ),
           );
         },
       );
